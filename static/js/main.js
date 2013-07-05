8d08a685d4d4d6e9608ffa71b6d5ec81d54dcc3e
Francisco Gomez <mail@frncscgmz.me>
2013-07-05 15:36:44 -0700

    Portafolio de imagenes con holder

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-	console.log("Hello!");
+   $('.photoset-grid-lightbox').photosetGrid({
+      highresLinks: true,
+   rel: 'withhearts-gallery',
+   gutter: '25px',
+
+   onComplete: function(){
+      $('.photoset-grid-lightbox').attr('style', '');
+      $('.photoset-grid-lightbox a').colorbox({
+         photo: true,
+         scalePhotos: true,
+         maxHeight:'90%',
+         maxWidth:'90%'
+      });
+   }
+   });
 });
